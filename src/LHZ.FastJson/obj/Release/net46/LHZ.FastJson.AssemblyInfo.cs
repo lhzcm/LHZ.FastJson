a1d059806bd2d3272ca4df5c57f262f26f6cbf01
lhzcm <lhzcm888888@gmail.com>
2020-09-15 21:21:56 +0800

    添加了新的发布版本

--- src/LHZ.FastJson/obj/Release/net46/LHZ.FastJson.AssemblyInfo.cs
+++ src/LHZ.FastJson/obj/Release/net46/LHZ.FastJson.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("轻巧便利的Json序列化和反序列化工具\r\nLightweight and convenient tool for JSON serialization and d" +
     "eserialization")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3.3")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.4.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3.4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LHZ.FastJson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LHZ.FastJson")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.3.3.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.3.4.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
